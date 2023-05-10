1aa0f72332e8d85cd3dff8e13aa8a166fd3711a9
Ram <Ram@RSPersonal>
2023-05-10 21:28:28 +0530

    Programs

---
+++ src/assignment/ArmstrongNum.java
@@ -0,0 +1,26 @@
+package assignment;
+
+public class ArmstrongNum
+{
+
+	public static void main(String[] args)
+	{
+		int number = 154, originalNumber, remainder, result = 0;
+
+        originalNumber = number;
+
+        while (originalNumber != 0)
+        {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, 3);
+            originalNumber /= 10;
+        }
+
+        if(result == number)
+            System.out.println(number + " is an Armstrong number.");
+        else
+            System.out.println(number + " is not an Armstrong number.");
+    }
+	
+
+}
---
+++ src/assignment/InfiniteLoop.java
@@ -0,0 +1,15 @@
+package assignment;
+
+public class InfiniteLoop {
+
+	public static void main(String[] args) 
+	{
+	
+		/* for (int i = 0; i < 2 ; i--) {
+		      System.out.println("Infinite");
+		    }*/
+	
+	}
+	
+
+}
