53399684e23f723d2ab156c044b0bbdeb1221c3b
Ram <Ram@RSPersonal>
2023-05-10 21:51:57 +0530

    program that reads a number and display the square, cube,fourth

---
+++ src/assignment/SquareCubePower.java
@@ -0,0 +1,18 @@
+package assignment;
+
+public class SquareCubePower {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int a =2;
+      int b=  a * a;
+      System.out.println("Square = " +b);
+      int c= a * a * a;
+      System.out.printf("Cube = " +c);
+      int d= a*a*a*a;
+      System.out.println("\nfourthpower=" +d);
+    /*  double d=  Math.pow(4);
+      System.out.printf("Fourth power"+d);*/
+	}
+
+}
